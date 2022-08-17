a15deae44c66a5e4295098139d517f6c1375bfb9
Sergei Nagaitsev <x64vs32x@mail.ru>
2022-08-17 20:23:17 +0300

    github publish action

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { series, dest, src } = require('gulp');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {series, dest, src} = require('gulp');
 const exec = require('child_process').exec;
+const argv = require('yargs').argv;
 
 function build(cb) {
     exec('npm run build', function (err, stdout, stderr) {
@@ -19,11 +18,14 @@ async function copy() {
 }
 
 function publish(cb) {
-    exec('cd dist && npm publish --access public', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+    exec(
+        `cd dist && npm version ${ argv.tag } --no-git-tag-version && npm publish --access public```,
+        function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            cb(err);
+        }
+    );
 }
 
 exports.default = series(build, copy, publish);
